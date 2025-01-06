1f7c04862b9580cde8b002c6a75c012614c43e94
BarbedMach <furkanozdemir.xyz@gmail.com>
2025-01-07 00:31:33 +0300

    hard::median-of-two-sorted-arrays

--- easy/two-sum/solution.go
+++ easy/two-sum/solution.go
@@ -1,23 +1,15 @@
 package main
 
-import "fmt"
-
 func twoSum(nums []int, target int) []int {
-    numMap := make(map[int]int)
+	numMap := make(map[int]int)
 
-    for i, num := range nums {
-        complement := target - num
-        if idx, found := numMap[complement]; found {
-            return []int{idx, i}
-        }
-        numMap[num] = i
-    }
-    
-    return nil
-}
+	for i, num := range nums {
+		complement := target - num
+		if idx, found := numMap[complement]; found {
+			return []int{idx, i}
+		}
+		numMap[num] = i
+	}
 
-func main() {
-    nums := []int{2, 7, 11, 15}
-    target := 9
-    fmt.Println(twoSum(nums, target))
+	return nil
 }
---
+++ hard/median-of-two-sorted-arrays/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # not the best solution, but the easiest one
+        # time complexity: O(nlogn)
+        # space complexity: O(n)
+        nums = sorted(nums1 + nums2)
+        n = len(nums)
+        if n % 2 == 0:
+            return (nums[n//2] + nums[n//2-1]) / 2
+        else:
+            return nums[n//2]
+        # O(log(m + n)) binary search solution is too complicated for me
+        # https://www.youtube.com/watch?v=q6IEA26hvXc
+
